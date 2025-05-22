a24a71a803ee55ba37f9386c1541921123c4766b
Ramadasu <ramadasu123@gmail.com>
2025-05-22 21:44:01 +0530

    changes in file2

--- file2.py
+++ file2.py
@@ -1,8 +1,9 @@
-def sub_numbers(x,y):
-    return x-y
+def sub_numbers(x,y,z):
+    return x-y-z
 
 
 
 x=int(input("Enter the first number: "))
 y=int(input("Enter the second number: "))
-print(sub_numbers(x,y))
+z=int(input("Enter the second number: "))
+print(sub_numbers(x,y,z))
